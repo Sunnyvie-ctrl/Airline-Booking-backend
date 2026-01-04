8c2e7795a89255f508261d896a144156c66926f4
Sunnyvie-ctrl <minh-nhat.bui@epita.fr>
2026-01-04 17:35:08 +0100

    added main back some pom configurations

---
+++ src/main/java/com/KORNN/airline_backend/AirlineBackendApplication.java
@@ -0,0 +1,15 @@
+package com.KORNN.airline_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main Spring Boot application entry point.
+ */
+@SpringBootApplication
+public class AirlineBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AirlineBackendApplication.class, args);
+    }
+}
+
