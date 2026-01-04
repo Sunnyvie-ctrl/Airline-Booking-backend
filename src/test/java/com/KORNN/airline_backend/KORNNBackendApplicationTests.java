34a22cddc3034439a9dabb898bc4dbd66b66b658
Sunnyvie-ctrl <minh-nhat.bui@epita.fr>
2026-01-04 17:18:25 +0100

    changing application propertise

---
+++ src/test/java/com/KORNN/airline_backend/KORNNBackendApplicationTests.java
@@ -0,0 +1,11 @@
+package com.KORNN.airline_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = com.KORNN.airline_backend.KORNNBackendApplicationTests.class)
+public class KORNNBackendApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
